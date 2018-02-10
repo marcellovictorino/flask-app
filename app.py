a8cb424b44556408fcc861700ef6f960d7c6d6c9
victorinoeng <mvictorino@sugarlandtx.gov>
2018-02-10 15:22:28 -0600

    file uploader added

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'uploaded/gpxFiles'
+ALLOWED_EXTENSIONS = set(['gpx', 'pdf'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
@@ -21,6 +27,48 @@ def equipmentCommStatus():
 @app.route("/gpx/")
 def gpx():
     return render_template('gpx.html')
+
+@app.route("/login/")
+def login():
+    return render_template('login.html')
+
+@app.route("/login_action/", methods=['POST', 'GET'])
+def login_action():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return "Welcome, {}".format(user)
+    else:
+        user = request.args.get('nm')
+        return "(GET) Welcome, {}".format(user)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/gpx_file_uploader/", methods = ['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        # if user does not select file, browser also submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # Do something to let user know file was uploaded succesfully
+            # add progress bar for file upload
+            # return redirect(url_for('upload_file', filename=filename)) # Why is this necessary!?
+        
+        return "File uploaded succesfully!"
+    else:
+        return 'Opsss... something wrong!'
 
 
 
